61b3d7e46b72f248b38dae2a66977263babc6f14
AdiDAgamer <61423116+AdiDAgamer@users.noreply.github.com>
2020-04-17 19:50:00 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,17 @@
+
+var peopleCount = 0;
+var database;
+
+function setup() 
+{
+  canvas = createCanvas(600, 600);
+  database = firebase.database();
+  form = new Form()
+  user = new User();
+}
+
+function draw()
+{
+  background(255);
+  form.display();
+}
